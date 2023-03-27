60ad641d0d77d0a034649aba1cba800c063ea56b
Dan Luo <luo_dana@yahoo.com>
2023-03-28 09:31:54 +1000

    Update AMM.sol

--- AMM.sol
+++ AMM.sol
@@ -61,14 +61,14 @@ contract AMM is AccessControl{
 		    swapA = sellAmount;
 		    qtyA = ERC20(tokenA).balanceOf(address(this));
 		    qtyB = ERC20(tokenB).balanceOf(address(this));
-		    swapAmt = qtyB - invariant/(qtyA+(1-feebps/10000)swapA)		    
+		    swapAmt = qtyB - invariant/(qtyA+(1-feebps/10000)*swapA);		    
 		    ERC20(tokenA).transferFrom(msg.sender, address(this), swapA);
 		    ERC20(tokenB).transfer(msg.sender, swapAmt);
 		} else {
 		    swapB = sellAmount;
 		    qtyA = ERC20(tokenA).balanceOf(address(this));		    
 		    qtyB = ERC20(tokenB).balanceOf(address(this));
-		    swapAmt = qtyA - invariant/(qtyB+(1-feebps/10000)swapB)
+		    swapAmt = qtyA - invariant/(qtyB+(1-feebps/10000)*swapB);
 		    ERC20(tokenB).transferFrom(msg.sender, address(this), swapB);
 		    ERC20(tokenA).transfer(msg.sender, swapAmt);
 		    }
